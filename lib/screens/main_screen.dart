d0b6ab7070d7d138be24dfc1b190c5e48b236e3a
vel <revelcahyadi2004@gmail.com>
2025-10-03 21:49:04 +0700

    feat (screen) : make the main screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
+import 'sky_info_screen.dart';
+import 'setting_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const DashboardScreen(),
+    const SkyInfoScreen(),
+    const SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.nights_stay),
+            label: 'Info Langit',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
