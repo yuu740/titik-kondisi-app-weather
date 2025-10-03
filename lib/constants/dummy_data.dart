af17465b3e38d94e5bd320e72fd5c963aa38d82c
vel <revelcahyadi2004@gmail.com>
2025-10-03 21:33:44 +0700

    feat (const) : dummy data

---
+++ lib/constants/dummy_data.dart
@@ -0,0 +1,19 @@
+class DummyData {
+  static const String location = 'South Tangerang, Banten';
+  static const String date =
+      'Friday, 03 October 2025 - 21:29'; // Updated to current date/time
+  static const double temperature = 29.0;
+  static const String weatherCondition = 'Light rain';
+  static const int aqi = 38;
+  static const int uv = 0;
+  static const int humidity = 72;
+  static const String rainPrediction =
+      'Hujan deras diperkirakan dalam 15 menit di area Anda. Siapkan payung!';
+  static const double observationScore = 9.1;
+  static const int cloudCover = 5;
+  static const String moonPhase = 'Baru';
+  static const String lightPollution = 'Minimum';
+  static const String bestTime = '21:45';
+
+  static List<double> hourlyRain = [0.8, 0.6, 0.4, 0.2, 0.1, 0.05];
+}
