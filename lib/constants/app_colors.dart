3f118efa8ab603401e3e5d96455345bd4cc4a514
vel <revelcahyadi2004@gmail.com>
2025-10-03 21:34:00 +0700

    feat (const) : app color based for consist color

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightPrimary = Color(0xFF87CEEB); // Sky Blue
+  static const Color lightBackground = Colors.white;
+  static const Color lightText = Colors.black87;
+  static const Color lightAccent = Color(0xFF00BFFF); // Deeper Blue
+
+  static const Color darkPrimary = Color(0xFF4B0082); // Indigo/Purple Dark
+  static const Color darkBackground = Color(0xFF1A1A1A); // Near Black
+  static const Color darkText = Colors.white;
+  static const Color darkAccent = Color(0xFF9370DB); // Medium Purple
+}
